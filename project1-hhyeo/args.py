dbd9c2a6e3310b5044928afd24ca7d3597bd2300
hyunho <chaos5958@gmail.com>
2017-11-08 11:32:25 +0900

    Project 1 done

--- project1-hhyeo/args.py
+++ project1-hhyeo/args.py
@@ -6,7 +6,7 @@ FLAGS= flags.FLAGS
 
 flags.DEFINE_integer('train_dataset_num', 10000, '')
 flags.DEFINE_integer('valid_dataset_num', 100, '')
-flags.DEFINE_integer('test_dataset_num', 10000, '')
+flags.DEFINE_integer('test_dataset_num', 1000, '')
 flags.DEFINE_integer('epoch', 1, '')
 flags.DEFINE_integer('batch_num', 128, '')
 flags.DEFINE_float('initial_lr', 1e-4, '')
--- project1-hhyeo/project1_task1.py
+++ project1-hhyeo/project1_task1.py
@@ -4,6 +4,9 @@ import argparse
 import random
 import args
 import sys
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 FLAGS = args.get()
 
@@ -26,6 +29,23 @@ def generate_dataset(num):
         label_list.append(label)
 
     train_data_list = [data_list, label_list]
+
+    """
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+    ax.set_xlabel('1st column')
+    ax.set_ylabel('2nd column')
+    ax.set_title('Classification')
+
+    for i in range(num):
+        if label_list[i][0,0] == 1:
+            ax.plot(data_list[i][0,0], data_list[i][0,1], 'C0o')
+        elif label_list[i][0,1] == 1:
+            ax.plot(data_list[i][0,0], data_list[i][0,1], 'C9o')
+
+    fig.savefig('task1-test.png')
+    plt.close(fig)
+    sys.exit(-1)
+    """
 
     return train_data_list
 
@@ -51,7 +71,7 @@ def build_model():
     y = h_1
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels = y_label, logits = y)
 
-    return x, y, y_label, cross_entropy
+    return x, y, h_0, y_label, cross_entropy
 
 def get_batch(train_dataset, index, batch_num):
     #if len(train_dataset) < index * args.batch_num
@@ -86,10 +106,8 @@ def main(not_parsed_args):
     valid_dataset = generate_dataset(FLAGS.valid_dataset_num)
     test_dataset = generate_dataset(FLAGS.test_dataset_num)
 
-    #print(len(train_dataset[0]))
-    #sys.exit(-1)
-
-    x, y, y_label, cross_entropy = build_model()
+    #build model
+    x, y, h_0, y_label, cross_entropy = build_model()
     optimizer = tf.train.AdamOptimizer(FLAGS.initial_lr).minimize(cross_entropy)
 
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_label, 1))
@@ -98,21 +116,91 @@ def main(not_parsed_args):
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
+        #training
         for i in range(FLAGS.epoch):
             shuffle_dataset(train_dataset[0], train_dataset[1])
 
             for j in range(int(len(train_dataset[0]) / FLAGS.batch_num)):
                 batch = get_batch(train_dataset, j, FLAGS.batch_num)
-                _, accuracy_ = sess.run([optimizer, accuracy], feed_dict={x: batch[0], y_label: batch[1]})
+                _ = sess.run([optimizer], feed_dict={x: batch[0], y_label: batch[1]})
 
                 #print(batch[0].shape)
 
-            print("[train] epoch: %d, step: %d, accuracy: %.3f"%(i, j, accuracy_))
+                if j%50==49:
+                    valid_data_arr = np.array(valid_dataset[0])
+                    valid_label_arr = np.array(valid_dataset[1])
+                    valid_data_arr = np.squeeze(valid_data_arr, axis=1)
+                    valid_label_arr = np.squeeze(valid_label_arr, axis=1)
+                    accuracy_train = sess.run(accuracy, feed_dict={x: batch[0], y_label: batch[1]})
+                    accuracy_valid = sess.run(accuracy, feed_dict={x: valid_data_arr, y_label: valid_label_arr})
+                    print("epoch: %d, step: %d, train-accuracy: %.3f valid-accuracy: %.3f"%(i+1, j+1, accuracy_train, accuracy_valid))
 
-            #accuracy = sess.run([accuracy], feed_dict={x: valid_dataset[0], y_label: valid_dataset[1]})
-            #print("[valid] epoch: %d, accuracy: %.3f"%(i, accuracy))
 
-    #TODO: validation set accuracy(task2 example code), hyper-parameter setting, test case plot
+        #test image plot
+        test_data_arr = np.array(test_dataset[0])
+        test_label_arr = np.array(test_dataset[1])
+        test_data_arr = np.squeeze(test_data_arr, axis=1)
+        test_label_arr = np.squeeze(test_label_arr, axis=1)
+
+        test_output,accuracy_ = sess.run([y, accuracy], feed_dict={x: test_data_arr, y_label: test_label_arr})
+        print("test-accuracy: %.3f"%(accuracy_))
+
+
+        hidden_layer_output = sess.run(h_0, feed_dict={x: test_data_arr, y_label: test_label_arr})
+        #print(hidden_layer_output.shape)
+
+        #line plotting (active region, inactive region)
+        line_x_inactive = [[] for x in range(np.size(hidden_layer_output, 1))]
+        line_y_inactive = [[] for x in range(np.size(hidden_layer_output, 1))]
+        line_x_active = [[] for x in range(np.size(hidden_layer_output, 1))]
+        line_y_active= [[] for x in range(np.size(hidden_layer_output, 1))]
+
+        #hidden_layer_sum = np.sum(hidden_layer_output, axis=1)
+        #print(hidden_layer_sum.shape)
+
+        for i in range(np.size(hidden_layer_output, 0)):
+            for j in range(np.size(hidden_layer_output, 1)):
+                if hidden_layer_output[i][j] == 0:
+                    line_x_inactive[j].append(test_data_arr[i][0])
+                    line_y_inactive[j].append(test_data_arr[i][1])
+                else:
+                    line_x_active[j].append(test_data_arr[i][0])
+                    line_y_active[j].append(test_data_arr[i][1])
+        """
+        for i in range(np.size(test_output, 0)):
+            if test_output[i, 0] - test_output[i, 1] > -0.01 and test_output[i, 0] - test_output[i, 1] < 0.01:
+                line_x_inactive.append(test_data_arr[i][0])
+                line_y_inactive.append(test_data_arr[i][1])
+
+        """
+
+        #test point plotting
+        for i in range(np.size(hidden_layer_output, 1)):
+            fig, ax = plt.subplots(nrows=1, ncols=1)
+            ax.set_xlabel('1st column')
+            ax.set_ylabel('2nd column')
+            ax.set_title('Boundary')
+
+            ax.plot(line_x_inactive[i], line_y_inactive[i], 'ro')
+            ax.plot(line_x_active[i], line_y_active[i], 'bo')
+
+            fig.savefig('task1-active-inactive-{}.png'.format(i))
+            plt.close(fig)
+
+        fig, ax = plt.subplots(nrows=1, ncols=1)
+        ax.set_xlabel('1st column')
+        ax.set_ylabel('2nd column')
+        ax.set_title('Classification')
+
+        for i in range(np.size(test_output, 0)):
+            if test_output[i, 0] > test_output[i, 1]:
+                ax.plot(test_data_arr[i][0], test_data_arr[i][1], 'ro')
+            elif test_output[i, 0] < test_output[i, 1]:
+                ax.plot(test_data_arr[i][0], test_data_arr[i][1], 'bo')
+        #ax.plot(line_x_inactive, line_y_inactive, 'og-')
+
+        fig.savefig('task1-classify.png')
+        plt.close(fig)
 
 if __name__ == '__main__':
     tf.app.run()
